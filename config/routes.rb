9c2327c7bd4e1a83bf5cf68bce3b832cea640c92
taha <taha.maqbool@talenthouse.com>
2023-01-20 12:11:33 +0500

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root :to => redirect('/videos')
-
+  root to: redirect('/videos')
   resources :videos
   resources :categories, only: [:index]
 end
